2114535db06f9f6e428ab0faa3eadec39666238e
Floydman <danielwellsfloyd@gmail.com>
2021-01-08 19:25:48 -0800

    Added license notice to all class files.

--- src/main/java/thefloydman/linkingbooks/tileentity/ModTileEntityTypes.java
+++ src/main/java/thefloydman/linkingbooks/tileentity/ModTileEntityTypes.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Linking Books
+ * Copyright (C) 2021  TheFloydman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can reach TheFloydman on Discord at Floydman#7171.
+ *******************************************************************************/
 package thefloydman.linkingbooks.tileentity;
 
 import com.google.common.collect.Sets;
