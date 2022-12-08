47f97d407de3fe027b7d4335452413ef7b1d1872
Floydman <danielwellsfloyd@gmail.com>
2022-12-07 20:20:56 -0800

    Updated copyright to LGPLv3.

--- src/main/java/thefloydman/linkingbooks/LinkingBooks.java
+++ src/main/java/thefloydman/linkingbooks/LinkingBooks.java
@@ -1,21 +1,20 @@
 /*******************************************************************************
- * Linking Books
- * Copyright (C) 2021  TheFloydman
+ * Copyright 2019-2022 Dan Floyd ("TheFloydman")
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This file is part of Linking Books.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Linking Books is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Linking Books is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
  *
- * You can reach TheFloydman on Discord at Floydman#7171.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Linking Books. If not, see <https://www.gnu.org/licenses/>.
  *******************************************************************************/
 package thefloydman.linkingbooks;
 
