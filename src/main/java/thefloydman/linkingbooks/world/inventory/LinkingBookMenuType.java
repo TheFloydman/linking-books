472ac98758162371575f0f016a35e6c7ab258afa
Floydman <22880199+TheFloydman@users.noreply.github.com>
2024-11-15 19:06:21 -0800

    Updated in-file license language.

--- src/main/java/thefloydman/linkingbooks/world/inventory/LinkingBookMenuType.java
+++ src/main/java/thefloydman/linkingbooks/world/inventory/LinkingBookMenuType.java
@@ -1,19 +1,19 @@
 /*
- * Copyright (c) 2019-2024 Dan Floyd ("TheFloydman").
+ * This file is part of Linking Books, a mod for Minecraft.
+ * Copyright (c) 2019-2019-2024 Dan Floyd ("TheFloydman").
  *
- * This file is part of Linking Books.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * Linking Books is free software: you can redistribute it and/or modify it under the terms
- * of the GNU Lesser General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ * the GNU Lesser General Public License for more details.
  *
- * Linking Books is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License along
- * with Linking Books. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package thefloydman.linkingbooks.world.inventory;
