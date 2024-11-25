3d39b68f29848914e7b253a8d9b8b00345b17e63
TheFloydman <22880199+TheFloydman@users.noreply.github.com>
2024-11-24 17:45:38 -0800

    Fixed DataComponents registry 1.21 -> 1.21.1.

--- src/main/java/thefloydman/linkingbooks/core/component/ModDataComponents.java
+++ src/main/java/thefloydman/linkingbooks/core/component/ModDataComponents.java
@@ -19,6 +19,7 @@
 package thefloydman.linkingbooks.core.component;
 
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import thefloydman.linkingbooks.data.LinkData;
@@ -26,7 +27,7 @@ import thefloydman.linkingbooks.util.Reference;
 
 public class ModDataComponents {
 
-    public static final DeferredRegister.DataComponents DATA_COMPONENTS = DeferredRegister.createDataComponents(Reference.MODID);
+    public static final DeferredRegister.DataComponents DATA_COMPONENTS = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, Reference.MODID);
 
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<LinkData>> LINK_DATA = DATA_COMPONENTS.registerComponentType(
             Reference.DataComponentNames.LINK_DATA, builder -> builder
